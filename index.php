f9ce552d66618346aa6f45238b9265b9302b3dc9
Madam Tinmay <rtinmay@gmail.com>
2024-12-15 20:07:14 +0800

    .

--- index.php
+++ index.php
@@ -8,64 +8,60 @@
 </head>
 <body>
     <div class="container my-5">
-        <h2>List of our Clients</h2>
-        <a class="btn btn-primary" href="/mycrud/create.php" role="button">New Client</a>
-        <br>
-        <table class="table">
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>Address</th>
-                <th>Created At</th>
-                <th>Action</th>
-            </tr>
-            <thead>
-                <tbody>
-                    <?php
-                    $servername = "localhost";
-                    $username = "root";
-                    $password = "";
-                    $database = "mycrud";
-
-                    $conn = new mysqli($servername, $username, $password, $database);
-
-                    if ($conn->connect_error) {
-                        die("Connection failed". $conn->connect_error);
-                    }
-
-                    $sql = "SELECT * FROM clients";
-                    $result = $conn->query($sql);
-
-                    if (!$result) {
-                        die("Invalid query". $conn->error);
-                    }
-
-                    while ($row = $result->fetch_assoc()) {
-                        echo"
-                        <tr>
-                            <td>$row[id]</td>
-                            <td>$row[name]</td>
-                            <td>$row[email]</td>
-                            <td>$row[phone]</td>
-                            <td>$row[address]</td>
-                            <td>$row[created_at]</td>
-                            <td>
-                                <a class='btn btn-primary btn-sm' href='/mycrud/edit.php?id=$row[id]'>Edit</a>
-                                <a class='btn btn-danger btn-sm' href='/mycrud/delete.php?id=$row[id]'>Delete</a>
-                            </td>
-                        </tr>
-                        ";
-                    }
-
-
-
-                    ?>
-                </tbody>
+         <h2>List of Clients</h2>
+         <a class="btn btn-primary" href="/mycrud/create.php" role="button">New Client</a>
+         <br>
+         <table class="table">
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                    <th>Created At</th>
+                    <th>Action</th>
+                </tr>
             </thead>
+            <tbody>
+                <?php
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $database = "sample";
 
-        </table>
+                $connection = new mysqli($servername, $username, $password, $database);
+
+                if ($connection->connect_error) {
+                    die("Connection failed: " . $connection->connect_error);
+                }
+
+                $sql = "SELECT * FROM clients";
+                $result = $connection->query($sql);
+
+                if (!$result) {
+                    die("Invalid query: ". $connection->error);
+                }
+
+                while ($row = $result->fetch_assoc()) {
+                    echo"
+                    <tr>
+                    <td>$row[id]</td>
+                    <td>$row[name]</td>
+                    <td>$row[email]</td>
+                    <td>$row[phone]</td>
+                    <td>$row[address]</td>
+                    <td>$row[created_at]</td>
+                    <td>
+                        <a class='btn btn-primary btn-sm' href='/mycrud/edit.php?id=$row[id]'>Edit</a>
+                        <a class='btn btn-danger btn-sm' href='/mycrud/delete.php?id=$row[id]'>Delete</a>
+                    </td>
+                </tr>
+                    ";
+                }
+                ?>
+            </tbody>
+         </table>
+
     </div>
 </body>
 </html>
