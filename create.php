6a5c295be24e454d93afd4fc669a7f3a012ceb9c
Madam Tinmay <rtinmay@gmail.com>
2024-12-15 20:49:39 +0800

    jshujds

--- create.php
+++ create.php
@@ -5,35 +5,35 @@ $username = "root";
 $password = "";
 $database = "mycrud";
 
-$conn = new mysqli($servername, $username, $password, $database);
+$connection = new mysqli($servername, $username, $password, $database);
 
 $name = "";
 $email = "";
 $phone = "";
 $address = "";
-
-$errorMessage = "";
+$errorMessage ="";
 $successMessage = "";
 
-if ( $_SERVER['REQUEST_METHOD'] == 'POST'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $name = $_POST["name"];
     $email = $_POST["email"];
-    $phome = $_POST["phone"];
+    $phone = $_POST["phone"];
     $address = $_POST["address"];
 
-    do {
-        if (empty($name) || empty($email) || empty($phome) || empty($address)){
+    do{
+        if ( empty($name) || empty($email) || empty($phone) || empty($address )){
             $errorMessage = "All the fields are required";
             break;
-    }
-
-    $sql = "INSERT INTO clients (name, email, phone, address)" . 
+        } 
+    //add new client to database
+    $sql = "INSERT INTO clients (name, email, phone, address)" .
             "VALUES ('$name', '$email', '$phone', '$address')";
     $result = $connection->query($sql);
 
-    if (!$result){
-        $errorMessage = "Invalid query. "; $conn->error;
+    if (!$result) {
+        $errorMessage = "Invalid query: ";  $connection->error;
         break;
+
     }
 
     $name = "";
@@ -44,11 +44,12 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST'){
     $successMessage = "Client added correctly";
 
     header("location: /mycrud/index.php");
-    exit();
+    exit;
 
-    }while(false);
+    } while (false);  
 
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -56,19 +57,20 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST'){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My CRUD</title>
+    <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
     <div class="container my-5">
         <h2>New Client</h2>
+
         <?php
-        if (!empty($errorMessage)) {
+        if (!empty($errorMessage)){
             echo"
             <div class='alert alert-warning alert-dismissible fade show' role='alert'>
-            <strong>$errorMessage</strong>
-            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                <strong>$errorMessage</strong>
+                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
             </div>
             ";
         }
@@ -76,41 +78,45 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST'){
         <form method="post">
             <div class="row mb-3">
                 <label class="col-sm-3 col-form-label">Name</label>
-                <div class="col sm-6">
+                <div class="col-sm-6">
                     <input type="text" class="form-control" name="name" value="<?php echo $name; ?>">
                 </div>
             </div>
+
             <div class="row mb-3">
                 <label class="col-sm-3 col-form-label">Email</label>
-                <div class="col sm-6">
+                <div class="col-sm-6">
                     <input type="text" class="form-control" name="email" value="<?php echo $email; ?>">
                 </div>
             </div>
+
             <div class="row mb-3">
                 <label class="col-sm-3 col-form-label">Phone</label>
-                <div class="col sm-6">
+                <div class="col-sm-6">
                     <input type="text" class="form-control" name="phone" value="<?php echo $phone; ?>">
                 </div>
             </div>
+
             <div class="row mb-3">
                 <label class="col-sm-3 col-form-label">Address</label>
-                <div class="col sm-6">
+                <div class="col-sm-6">
                     <input type="text" class="form-control" name="address" value="<?php echo $address; ?>">
                 </div>
             </div>
 
             <?php
-            if (!empty($successMessage)) {
+            if (!empty($successMessage)){
                 echo "
                 <div class='row mb-3'>
                     <div class='offset-sm-3 col-sm-6'>
-                        <div class='alert alert-success alert-dismissible fade show' role='alert'>
-                            <strong>$successMessage</strong>
-                            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                        <div class='alert alert-succes alert-dismissible fade show' role='alert'>
+                        <strong>$successMessage</strong>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
                         </div>
                     </div>
                 </div>
                 ";
+
             }
             ?>
 
@@ -119,7 +125,7 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST'){
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
                 <div class="col-sm-3 d-grid">
-                    <a class="btn btn-outline-primary" href="/mycrud/index.php" role="button">Cancel</a>
+                    <a class="btn btn-outline-primary" href="/mycrud/index.php" role="button" >Cancel</a>
                 </div>
             </div>
         </form>
