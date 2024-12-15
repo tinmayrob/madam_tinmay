e491186cd2f5d4d421ab327b5408b58a451058bd
Madam Tinmay <rtinmay@gmail.com>
2024-12-15 20:44:06 +0800

    .

--- index.php
+++ index.php
@@ -1,67 +1,68 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My CRUD</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-</head>
-<body>
+ </head>
+ <body>
     <div class="container my-5">
-         <h2>List of Clients</h2>
-         <a class="btn btn-primary" href="/mycrud/create.php" role="button">New Client</a>
-         <br>
-         <table class="table">
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Address</th>
-                    <th>Created At</th>
-                    <th>Action</th>
-                </tr>
+        <h2>List of my Clients</h2>
+        <a class="btn btn-primary" href="/mycrud/create.php" role="button">New Client</a>
+        <br>
+
+        <table class="table">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Address</th>
+                <th>Created At</th>
+                <th>Action</th>
+            </tr>
+            <thead>
+                <tbody>
+                    <?php
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $database = "mycrud";
+
+                    $conn = new mysqli($servername, $username, $password, $database);
+
+                    if ($conn->connect_error) {
+                        die("Connection failed. ". $conn->connect_error);
+                    }
+
+                    $sql = "SELECT * FROM clients";
+                    $result = $conn->query($sql);
+
+                    if (!$result) {
+                        die("Invalid query. ". $conn->connect_error);
+                    }
+
+                    while($row = $result->fetch_assoc()) {
+                        echo"
+                        <tr>
+                            <td>$row[id]</td>
+                            <td>$row[name]</td>
+                            <td>$row[email]</td>
+                            <td>$row[phone]</td>
+                            <td>$row[address]</td>
+                            <td>$row[created_at]</td>
+                            <td>
+                                <a class='btn btn-primary btn-sm' href='/mycrud/edit.php?id=$row[id]'>Edit</a>
+                                <a class='btn btn-danger btn-sm' href='/mycrud/delete.php?id=$row[id]'>Delete</a>
+                            </td>
+                        </tr>
+                        ";
+                    }
+                    ?>
+                </tbody>
             </thead>
-            <tbody>
-                <?php
-                $servername = "localhost";
-                $username = "root";
-                $password = "";
-                $database = "sample";
-
-                $connection = new mysqli($servername, $username, $password, $database);
-
-                if ($connection->connect_error) {
-                    die("Connection failed: " . $connection->connect_error);
-                }
-
-                $sql = "SELECT * FROM clients";
-                $result = $connection->query($sql);
-
-                if (!$result) {
-                    die("Invalid query: ". $connection->error);
-                }
-
-                while ($row = $result->fetch_assoc()) {
-                    echo"
-                    <tr>
-                    <td>$row[id]</td>
-                    <td>$row[name]</td>
-                    <td>$row[email]</td>
-                    <td>$row[phone]</td>
-                    <td>$row[address]</td>
-                    <td>$row[created_at]</td>
-                    <td>
-                        <a class='btn btn-primary btn-sm' href='/mycrud/edit.php?id=$row[id]'>Edit</a>
-                        <a class='btn btn-danger btn-sm' href='/mycrud/delete.php?id=$row[id]'>Delete</a>
-                    </td>
-                </tr>
-                    ";
-                }
-                ?>
-            </tbody>
-         </table>
-
+        </table>
     </div>
-</body>
-</html>
+ </body>
+ </html>
